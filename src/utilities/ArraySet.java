c0210737d7856344d2dffd5dc203d855e85fe9b8
joshberger5 <joshberger5@yahoo.com>
2023-01-10 15:24:36 -0500

    added implementation

--- src/utilities/ArraySet.java
+++ src/utilities/ArraySet.java
@@ -1,11 +1,162 @@
 package utilities;
 
-public class ArraySet
-{
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Set;
+import java.util.Spliterator;
 
-	public ArraySet()
-	{
-		// TODO Auto-generated constructor stub
+public class ArraySet<E> implements List<E>, Set<E>
+{
+	protected ArrayList<E> _list;
+	
+	public ArraySet() {
+		
 	}
 
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return _list.size();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return _list.isEmpty();
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		// TODO Auto-generated method stub
+		return _list.contains(o);
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		// TODO Auto-generated method stub
+		return _list.iterator();
+	}
+
+	@Override
+	public Object[] toArray() {
+		// TODO Auto-generated method stub
+		return _list.toArray();
+	}
+
+	@Override
+	public <T> T[] toArray(T[] a) {
+		// TODO Auto-generated method stub
+		return _list.toArray(a);
+	}
+
+	@Override
+	public boolean add(E e) {
+		// TODO Auto-generated method stub
+		return _list.add(e);
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		// TODO Auto-generated method stub
+		return _list.remove(o);
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return _list.containsAll(c);
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends E> c) {
+		// TODO Auto-generated method stub
+		return _list.addAll(c);
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return _list.retainAll(c);
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return _list.removeAll(c);
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		_list.clear();
+	}
+
+	@Override
+	public boolean addAll(int index, Collection<? extends E> c) {
+		// TODO Auto-generated method stub
+		return _list.addAll(index, c);
+	}
+
+	@Override
+	public E get(int index) {
+		// TODO Auto-generated method stub
+		return _list.get(index);
+	}
+
+	@Override
+	public E set(int index, E element) {
+		// TODO Auto-generated method stub
+		return _list.set(index, element);
+	}
+
+	@Override
+	public void add(int index, E element) {
+		// TODO Auto-generated method stub
+		_list.add(index, element);
+	}
+
+	@Override
+	public E remove(int index) {
+		// TODO Auto-generated method stub
+		return _list.remove(index);
+	}
+
+	@Override
+	public int indexOf(Object o) {
+		// TODO Auto-generated method stub
+		return _list.indexOf(o);
+	}
+
+	@Override
+	public int lastIndexOf(Object o) {
+		// TODO Auto-generated method stub
+		return _list.lastIndexOf(o);
+	}
+
+	@Override
+	public ListIterator<E> listIterator() {
+		// TODO Auto-generated method stub
+		return _list.listIterator();
+	}
+
+	@Override
+	public ListIterator<E> listIterator(int index) {
+		// TODO Auto-generated method stub
+		return _list.listIterator(index);
+	}
+
+	@Override
+	public List<E> subList(int fromIndex, int toIndex) {
+		// TODO Auto-generated method stub
+		return _list.subList(fromIndex, toIndex);
+	}
+
+	@Override
+	public Spliterator<E> spliterator() {
+		// TODO Auto-generated method stub
+		return List.super.spliterator();
+	}
 }
